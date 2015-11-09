aa67c2e1634c6379318dd056123a87c3d8dbc719
luis-rico <luis.rico70@gmail.com>
2015-11-09 17:19:34 +1100

    Query to MongoDB for selected fields
    Trying a structure in dashboard

--- main_mongodb.py
+++ main_mongodb.py
@@ -18,9 +18,23 @@ DBS_NAME = 'MTMbogie'
 COLLECTION_NAME = 'projects'
 FIELDS = {'Analysis Type': True, 'Accelerometer Location': True, 'Speed Bracket': True, 'Route': True, 'Nearest Station': True, 'Exceedence Instance': True, 'Time':True, '_id': False}
 
+
+##DBS_NAME2 = 'experall'
+DBS_NAME2 = 'smallall'
+FIELDS2 = {'Speed': True, 'Speed Bracket': True, 'Nearest Station': True, 'Exceedence Instance': True, 'Time':True, '_id': False}
+
+FIELDS3 = {'Speed': True, 'Analysis Type': True, 'Accelerometer Location': True, 'Speed Bracket': True, 'Route': True, 'Nearest Station': True, 'Exceedence Instance': True, 'Time':True, '_id': False}
+
+
+## This works alongside @app.route("/MTMBogieMongo")
 @app.route("/")
 def index():
     return render_template("MTMdashboardMongoDB.html")
+	
+## This works alongside @app.route("/experall")
+@app.route("/2")
+def index2():
+    return render_template("MTMdashboardMongoDBall.html")
 
 
 @app.route("/MTMBogieMongo")
@@ -28,6 +42,19 @@ def MTMBogieMongo():
     connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
     collection = connection[DBS_NAME][COLLECTION_NAME]
     projects = collection.find(projection=FIELDS)
+    json_projects = []
+    for project in projects:
+        json_projects.append(project)
+    json_projects = json.dumps(json_projects, default=json_util.default)
+    connection.close()
+    return json_projects
+	
+@app.route("/experall")
+def experall():
+    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+    collection = connection[DBS_NAME2][COLLECTION_NAME]
+    ##projects = collection.find(projection=FIELDS2, limit=100000)
+    projects = collection.find(projection=FIELDS3)
     json_projects = []
     for project in projects:
         json_projects.append(project)
---
+++ static/chart2.js
@@ -0,0 +1,205 @@
+queue()
+    .defer(d3.json, "/experall")
+    .await(makeGraphs);
+	
+function makeGraphs(error, data) {
+
+// Everything is imported as a string by default. Numeric column in read data must be converter to numeric
+// Columns with numeric	values must be processed below			
+		var convert_2_int = function() {
+			var max_length = data.length; 
+				for (var i=0; i < max_length; i++) {
+					//data[i]["Value"] = parseFloat(data[i]["Value"]);
+					//data[i]["Frequency(Hz)"] = parseFloat(data[i]["Frequency(Hz)"]);
+					data[i]["Time"] = parseFloat(data[i]["Time"]);
+					data[i]["Speed"] = parseFloat(data[i]["Speed"]);
+					data[i]["Exceedence Instance"] = parseInt(data[i]["Exceedence Instance"]);
+				}
+		}
+		convert_2_int();
+
+ //Date format needs to be parsed and and the same time, an aggregate field can be processed
+
+	//	var parseDate = d3.time.format("%d/%m/%Y").parse;  // LHR this is a function expression
+	//		data.forEach(function(d) {		// LHR d only exists whilst forEach is being executed.
+	//		d.Exceedence_Date = parseDate(d.Date);
+			
+	//	});
+		
+var ndx = crossfilter(data);
+
+var all = ndx.groupAll();
+
+// count all the detections
+		dc.dataCount(".dc-data-count")
+			.dimension(ndx)
+			.group(all);			
+
+// Line chart code
+			
+		//var dateDim = ndx.dimension(function(d) {return d.Detection_Date;}); 	// Note that we are grouping by month to match CMT excel plots
+		var timeDim = ndx.dimension(function(d) {return d.Time;}); 
+		
+		var minTime = timeDim.bottom(1)[0].Time;
+		var maxTime = timeDim.top(1)[0].Time;
+		
+		
+		lineChart  = dc.lineChart("#line-chart");   // Note that 'var' has been removed in order to be able to reset chart
+													// Full explanation in http://stackoverflow.com/questions/21550270/dc-js-unable-to-redraw-charts (see jfiddle)
+													
+		rangeChart = dc.barChart('#range-chart');	// Note that 'var' has been removed in order to be able to reset chart
+		
+		//var Excedeence_Count=timeDim.group().reduceSum(function(d)
+			//{return d['Exceedence Instance'];} );
+		
+		var timeDimGroup = timeDim.group();
+		
+		var SpeedGroup = timeDimGroup.reduce(function(d)
+			{return d['Speed'];} );
+			
+		// var tooltipDateFormat = d3.time.format("%a %e %b %Y");
+		var tooltipDateFormat = d3.time.format("%b %Y");
+		
+		lineChart                       
+			.width(750).height(205)
+			.dimension(timeDim)
+			//.group(Excedeence_Count)
+			.group(SpeedGroup)
+			.renderArea(true)
+			.renderHorizontalGridLines(true)
+			.renderVerticalGridLines(true)
+			//.x(d3.time.scale().domain([minTime,maxTime]))
+			.x(d3.scale.linear().domain([minTime, maxTime]))
+			.brushOn(false)
+			//.title(function(d){						// tooltips are shown by using the title function. Also, brushOn must be set to false
+			//  return tooltipDateFormat(d.data.key) + "\nNumber of Exceedences: " + d.data.value; })  // Note that data, key and value are reserved words for the function and do not correspond to fields in the imported data
+			.elasticY(true)
+			.rangeChart(rangeChart)
+			.yAxisLabel("Exceedence Count");
+			
+		rangeChart
+		    .width(750).height(70)
+			//.margins({top: 0, right: 0, bottom: 20, left: 0})
+			.dimension(timeDim)
+			//.group(Excedeence_Count)
+			.group(SpeedGroup)
+			.renderHorizontalGridLines(true)
+			.renderVerticalGridLines(true)
+			//.centerBar(true)
+			//.gap(1)
+			//.x(d3.time.scale().domain([minTime,maxTime]))
+			.x(d3.scale.linear().domain([minTime, maxTime]))
+			.yAxisLabel("")       // This is to blank out the yAxis labels
+			.yAxis().tickFormat(function(v) { return ""; });  // This is to blank out the yAxis scale
+			
+// Row chart code
+
+		locationRowChart = dc.rowChart("#row-chart");		// Note that 'var' has been removed in order to be able to reset chart
+		var locationDimension = ndx.dimension(function(d) {return d["Nearest Station"];});
+		var locationGroup = locationDimension.group();
+		
+		locationGroupRow = locationGroup.reduceSum(function(d) {return d['Exceedence Instance'];});
+
+		var filtered_group = filter_top(locationGroupRow);
+		function filter_top(locationGroupRow) {
+		  return {
+			all:function () {
+				return locationGroupRow.top(10);
+				}
+			}
+		 }
+			
+	    locationRowChart
+			.width(280)
+			.height(300)
+			.margins({top: 20, left: 10, right: 10, bottom: 20})
+			.dimension(locationDimension)
+			.group(filtered_group)
+			.ordering(function(d) {return -d.value})
+			.title(function (d) {
+				return d.value;
+			})
+			.elasticX(true)
+			.xAxis().ticks(5);
+
+// Row chart code 2
+
+		routeRowChart = dc.rowChart("#row-chart2");		// Note that 'var' has been removed in order to be able to reset chart
+		var routeDimension = ndx.dimension(function(d) {return d["Route"];});
+		var routeGroup = routeDimension.group();
+		
+		routeGroupRow = routeGroup.reduceSum(function(d) {return d['Exceedence Instance'];});
+
+		var filtered_group2 = filter_top2(routeGroupRow);
+		function filter_top2(routeGroupRow) {
+		  return {
+			all:function () {
+				return routeGroupRow.top(10);
+				}
+			}
+		 }
+			
+	    routeRowChart
+			.width(280)
+			.height(300)
+			.margins({top: 20, left: 10, right: 10, bottom: 20})
+			.dimension(routeDimension)
+			.group(filtered_group2)
+			.ordering(function(d) {return -d.value})
+			.title(function (d) {
+				return d.value;
+			})
+			.elasticX(true)
+			.xAxis().ticks(5);
+			
+			
+// Ring chart 2 code
+			
+		dateRingChart2   = dc.pieChart("#ring-chart2");			// Note that 'var' has been removed in order to be able to reset chart
+		
+		analysisDim = ndx.dimension(function(d) {return d['Analysis Type'];});
+		
+		var analysis_total = analysisDim.group().reduceSum(function(d) {return d['Exceedence Instance'];});
+
+		dateRingChart2
+			.width(150)
+			.height(150)
+			.dimension(analysisDim)
+			.group(analysis_total)
+			.innerRadius(30);
+			
+// Ring chart 3 code
+			
+		dateRingChart3   = dc.pieChart("#ring-chart3");			// Note that 'var' has been removed in order to be able to reset chart
+		
+		accelerometerDim = ndx.dimension(function(d) {return d['Accelerometer Location'];});
+		
+		var accelerometer_total = accelerometerDim.group().reduceSum(function(d) {return d['Exceedence Instance'];});
+
+		dateRingChart3
+			.width(150)
+			.height(150)
+			.dimension(accelerometerDim)
+			.group(accelerometer_total)
+			.innerRadius(30);
+			
+// Ring chart 4 code
+			
+		speedRingChart   = dc.pieChart("#ring-chart4");			// Note that 'var' has been removed in order to be able to reset chart
+		
+		speedDim = ndx.dimension(function(d) {return d['Speed Bracket'];});
+		
+		var speed_total = speedDim.group().reduceSum(function(d) {return d['Exceedence Instance'];});
+
+		speedRingChart
+			.width(150)
+			.height(150)
+			.dimension(speedDim)
+			.group(speed_total)
+			.innerRadius(30);
+	
+	
+		dc.renderAll();
+};
+	
+	
