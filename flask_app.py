f1339f464b109bad2f9ed7850eb990d95e73e593
luis-rico <luis.rico70@gmail.com>
2016-01-19 21:36:53 +1100

    Fully working solution for single js-html code and local or cloud Db
    Success!!!!

--- flask_app.py
+++ flask_app.py
@@ -8,8 +8,6 @@ from bson.json_util import dumps
 
 ## Tutorial can be found in
 ## http://adilmoujahid.com/posts/2015/01/interactive-data-visualization-d3-dc-python-mongodb/
-## Data sent to MongoDB is from: exceedancesLHR-pres.tsv
-
 
 app = Flask(__name__)
 
@@ -19,92 +17,7 @@ DBS_NAME = 'mtm'
 USER = 'lhr'
 PASS = 'ferrari1'
 COLLECTION_NAME = 'projects'
-FIELDS = {'Analysis Type': True, 'Accelerometer Location': True, 'Speed Bracket': True, 'Route': True, 'Nearest Station': True, 'Exceedence Instance': True, 'Time':True, '_id': False}
 
-##DBS_NAME2 = ''
-##DBS_NAME2 = 'experall'
-##DBS_NAME2 = 'smallall'
-##DBS_NAME2 = 'smallseries'
-##DBS_NAME2 = 'smallseriesdays'
-##DBS_NAME2 = 'allIndays'
-##DBS_NAME2 = 'allIndaysCons'
-##DBS_NAME2 = 'allIndaysCons3'
-##DBS_NAME2 = 'allIndaysCons3Redu'
-##DBS_NAME2 = 'allIndaysCons3Redu2'
-##DBS_NAME2 = 'allIndaysCons4'
-##DBS_NAME2 = 'AllWerribeeIn4Mongodb'
-FIELDS2 = {'Speed': True, 'Speed Bracket': True, 'Nearest Station': True, 'Exceedence Instance': True, 'Time':True, '_id': False}
-
-FIELDS3 = {'Speed': True, 'Analysis Type': True, 'Accelerometer Location': True, 'Speed Bracket': True, 'Route': True, 'Nearest Station': True, 'Exceedence Instance': True, 'Time':True, 'Series':True, 'Date':True, 'id': True}
-
-FIELDS4 = {'Speed': True, 'Analysis Type': True, 'Accelerometer Location': True, 'Speed Bracket': True, 'Route': True, 'Nearest Station': True, 'Exceedence Instance': True, 'Date_Time':True, 'Series':True, 'id': True}
-FIELDS5 = {'Speed': True, 'Analysis Type': True, 'Accelerometer Location': True, 'Speed Bracket': True, 'Route': True, 'Nearest Station': True, 'Exceedence Instance': True, 'Series':True, 'Time': True, 'Run': True,}
-
-
-## This works alongside @app.route("/MTMBogieMongo")
-@app.route("/")
-def index():
-    return render_template("MTMdashboardMongoDB.html")
-	
-## This works alongside @app.route("/experall")
-@app.route("/2")
-def index2():
-    return render_template("MTMdashboardMongoDBall.html")
-	
-## This works alongside @app.route("/experall") & chart3.js
-## DBS_NAME2 should be as 'smallseries'
-@app.route("/3")
-def index3():
-	
-    return render_template("MTMdashboardMongoDBsmallscatter.html")
-	
-## This works alongside @app.route("/experall") & chart4.js
-## DBS_NAME2 should be as 'smallseriesdays'
-@app.route("/4")
-def index4():
-	##global DBS_NAME2 = 'smallseriesdays'
-    return render_template("MTMdashboardMongoDBsmallscatterdays.html")
-	
-## This works alongside @app.route("/experall") & chart4.js
-## DBS_NAME2 should be as 'allIndays'
-@app.route("/5")
-def index5():
-    return render_template("MTMdashboardMongoDBsmallscatterdays.html")
-	
-## This works alongside @app.route("/experall") & chart5.js
-## DBS_NAME2 should be as 'allIndaysCons'
-## FIELDS4 should be used
-@app.route("/6")
-def index6():
-    return render_template("MTMdashboardMongoDBCons.html")
-
-	
-## This works alongside @app.route("/experall") & chart6.js
-## DBS_NAME2 should be as 'allIndaysCons3'
-## FIELDS5 should be used
-## Date_Time concept flawed. Run column created for filtering instead of date. Speed vs Time in Line Chart and series as per Series column
-@app.route("/7")
-def index7():
-    return render_template("MTMdashboardMongoDBCons2.html")
-
-	
-## This works alongside @app.route("/experall") & chart7.js
-## DBS_NAME2 should be as 'allIndaysCons3'
-## FIELDS5 should be used
-## Time based. Run column created for filtering instead of date. Speed vs Time in Line Chart and series as per Series column
-@app.route("/8")
-def index8():
-    ##return render_template("MTMdashboardTimeRun_Temp.html")
-	return render_template("MTMdashboardTimeRun.html")
-	
-## This works alongside @app.route("/experall") & chart7.js
-## DBS_NAME2 should be as 'allIndaysCons4'
-## FIELDS5 should be used
-## All widgets in. Database populate with "Normal Data" for blanks in Analysis Type, Accelerometer Location & Speed Bracket.
-@app.route("/9")
-def index9():
-	return render_template("MTMdashboardTimeRun.html")
-	
 	
 ## #############################################
 ##                                            ##
@@ -120,34 +33,19 @@ def index9():
 @app.route("/MTMBogie")
 def MTMBogie():
 	return render_template("MTMdashboardTimeRun.html")
+
+@app.route("/MTMBogie/local")
+def MTMBogieLocal():
+	return render_template("MTMdashboard2Dbs.html", mode='local')
+
+@app.route("/MTMBogie/cloud")
+def MTMBogieCloud():
+	return render_template("MTMdashboard2Dbs.html", mode='cloud')
 	
 	
-@app.route("/g")
-def indexg():
-    return render_template("multi-scatter&multi-line_for_series_value_non_sum.html")
-
-@app.route("/g2")
-def indexg2():
-    return render_template("multi-scatter&multi-line_for_series_value_non_sum_mongoDB.html")
-
-
-@app.route("/MTMBogieMongo")
-def MTMBogieMongo():
-    connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    collection = connection[DBS_NAME][COLLECTION_NAME]
-    projects = collection.find(projection=FIELDS)
-    json_projects = []
-    for project in projects:
-        json_projects.append(project)
-    json_projects = json.dumps(json_projects, default=json_util.default)
-    connection.close()
-    return json_projects
-	
-@app.route("/experall")
-def experall():
-    ##connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
-    ##collection = connection[DBS_NAME][COLLECTION_NAME]
-    ##collection.the_database.authenticate(USER, PASS, mechanism='SCRAM-SHA-1')
+@app.route("/connect2MongoDbCloud")
+def MongoDbCloud():
+    FIELDS5 = {'Speed': True, 'Analysis Type': True, 'Accelerometer Location': True, 'Speed Bracket': True, 'Route': True, 'Nearest Station': True, 'Exceedence Instance': True, 'Series':True, 'Time': True, 'Run': True,}
     mongoDBClient = MongoClient(MONGODB_HOST, MONGODB_PORT)
     mongoDB = mongoDBClient[DBS_NAME]
     mongoDB.authenticate(USER, PASS)
---
+++ static/processGraphs.js
@@ -0,0 +1,213 @@
+	
+function makeGraphs(error, data) {
+	
+
+
+// Everything is imported as a string by default. Numeric column in read data must be converter to numeric
+// Columns with numeric	values must be processed below			
+		var convert_2_int = function() {
+			var max_length = data.length; 
+				for (var i=0; i < max_length; i++) {
+					data[i]["Time"] = parseFloat(data[i]["Time"]);
+					data[i]["Speed"] = parseFloat(data[i]["Speed"]);
+					data[i]["Series"] = parseInt(data[i]["Series"]);
+					data[i]["Run"]= parseInt(data[i]["Run"]);
+					data[i]["Exceedence Instance"] = parseInt(data[i]["Exceedence Instance"]);
+				}
+		}
+		convert_2_int();
+
+ //Date parsing code has been removed from here
+ 
+var ndx = crossfilter(data);
+
+var all = ndx.groupAll();
+
+// count all the data rows. Note that exceedences only can not be counted
+		dc.dataCount(".dc-data-count")
+			.dimension(ndx)
+			.group(all);		
+
+
+      scatterDimension    = ndx.dimension(function(d) { return [+d.Time, +d.Speed ]; }),
+      scatterGroup1        = scatterDimension.group().reduceSum(function(d) { return +d.Series === 1; }),
+	  scatterGroup2        = scatterDimension.group().reduceSum(function(d) { return +d.Series === 2; }),
+  
+	// Code for Dimension that can separate Series for Line Chart
+	  timeDimension        = ndx.dimension(function(d) {return +d.Time;});  // This dimension is needed for minTime and maxTime as scatterDimension is not working
+	  
+
+			
+// Composite chart code
+			
+		var minTime = timeDimension.bottom(1)[0].Time;
+		var maxTime = timeDimension.top(1)[0].Time;
+			
+			
+var chart = dc.compositeChart("#line-chart");
+		
+
+		chart
+			.width(750)
+			.height(300)
+			//.elasticX(true)
+			.x(d3.scale.linear().domain([minTime, maxTime]))
+			.yAxisLabel("Speed")
+			.xAxisLabel("Time")
+			.clipPadding(10)
+			.brushOn(false)
+			.elasticY(true)
+			.dimension(scatterDimension)
+			.legend(dc.legend().x(625).y(5).itemHeight(13).gap(5))
+			.compose([
+				dc.scatterPlot(chart)
+                .group(scatterGroup1, "Normal Data")
+                .colors("blue"),
+				dc.scatterPlot(chart)
+                .group(scatterGroup2, "Exceedences")
+                .colors("red"),
+			]);		  
+
+		  
+// Row chart code
+
+		locationRowChart = dc.rowChart("#row-chart");		// Note that 'var' has been removed in order to be able to reset chart
+		var locationDimension = ndx.dimension(function(d) {return d["Nearest Station"];});
+		var locationGroup = locationDimension.group();
+		
+		locationGroupRow = locationGroup.reduceSum(function(d) {return d['Exceedence Instance'];});
+
+		var filtered_group = filter_top(locationGroupRow);
+		function filter_top(locationGroupRow) {
+		  return {
+			all:function () {
+				return locationGroupRow.top(10);
+				}
+			}
+		 }
+			
+	    locationRowChart
+			.width(280)
+			.height(300)
+			.margins({top: 20, left: 10, right: 10, bottom: 20})
+			.dimension(locationDimension)
+			.group(filtered_group)
+			.ordering(function(d) {return -d.value})
+			.title(function (d) {
+				return d.value;
+			})
+			.elasticX(true)
+			.xAxis().ticks(5);		  
+	  
+		  
+// Ring Run chart code
+			
+		runRingChart   = dc.pieChart("#ring-RunChart");			// Note that 'var' has been removed in order to be able to reset chart
+		
+		RunDim = ndx.dimension(function(d) {return d['Run'];});
+		
+		var RunGroup = RunDim.group();
+		
+		runRingChart
+			.width(150)
+			.height(150)
+			.dimension(RunDim)
+			.group(RunGroup)
+			.innerRadius(30)
+			.label(function (d){
+				return d.key;
+			})
+			.renderLabel(true)
+			.renderTitle(true);
+			
+// Analysis chart code
+	
+		AnalysisChart   = dc.rowChart("#AnalisysChart");			// Note that 'var' has been removed in order to be able to reset chart
+		
+		AnalysisDim = ndx.dimension(function(d) {return d['Analysis Type'];});
+		
+		var AnalysisGroup = AnalysisDim.group();
+		
+		AnalysisChart
+			.width(280)
+			.height(300)
+			.margins({top: 20, left: 10, right: 10, bottom: 20})
+			.dimension(AnalysisDim)
+			.group(AnalysisGroup)
+			.ordering(function(d) {return -d.value})		
+			.title(function (d) {
+				return d.value;
+			})
+			.elasticX(true)
+			.xAxis().ticks(5);
+			
+// Accelerometer Location chart code
+	
+		AccLocationChart   = dc.rowChart("#accLocationChart");			// Note that 'var' has been removed in order to be able to reset chart
+		
+		AccLocationDim = ndx.dimension(function(d) {return d['Accelerometer Location'];});
+		
+		var AccLocationGroup = AccLocationDim.group();
+		
+		AccLocationChart
+			.width(280)
+			.height(300)
+			.margins({top: 20, left: 10, right: 10, bottom: 20})
+			.dimension(AccLocationDim)
+			.group(AccLocationGroup)
+			.ordering(function(d) {return -d.value})		
+			.title(function (d) {
+				return d.value;
+			})
+			.elasticX(true)
+			.xAxis().ticks(5);	
+
+			
+// Ring chart 4 code
+			
+		SpeedBracketChart   = dc.rowChart("#speedBracketChart");			// Note that 'var' has been removed in order to be able to reset chart
+		
+		SpeedBracketDim = ndx.dimension(function(d) {return d['Speed Bracket'];});
+		
+		var SpeedBracketGroup = SpeedBracketDim.group();
+
+		SpeedBracketChart
+			.width(280)
+			.height(300)
+			.margins({top: 20, left: 10, right: 10, bottom: 20})
+			.dimension(SpeedBracketDim)
+			.group(SpeedBracketGroup)
+			.ordering(function(d) {return -d.value})		
+			.title(function (d) {
+				return d.value;
+			})
+			.elasticX(true)
+			.xAxis().ticks(5);	
+		
+		
+		  
+		dc.renderAll();
+		
+		
+// DropDown list code
+		
+	d3.select('#myDropDown').on('change', function(){ 
+		console.log(this.value);
+		
+		var strWindowFeatures = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+		if (this.value==1) {
+			window.open("/static/Werribee_Route_4_th_6.html", "Route 4th - 6", strWindowFeatures);
+		};
+		if (this.value==2) {
+			window.open("/static/Werribee_Route_23rd_7.html", "Route 23rd - 7", strWindowFeatures);
+		};
+		if (this.value==3) {
+			window.open("/static/Werribee_Route_29th_5.html", "Route 29th - 5", strWindowFeatures);
+		};
+		if (this.value==4) {
+			window.open("/static/Werribee_Route_29th_7.html", "Route 29th - 7", strWindowFeatures);
+		};
+	});
+};
+	
+	
