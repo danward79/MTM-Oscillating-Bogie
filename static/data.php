ab128e260db6ca790e9e3a02599c944b41aaa18b
luis-rico <luis.rico70@gmail.com>
2016-02-04 16:54:43 +1100

    TS109Pro version using php to read db in NAS
    Working although scaling is not full up to 120km/h. Bizarre!
    Note that d3.json is being used to call php script which connects to db

---
+++ static/data.php
@@ -0,0 +1,29 @@
+<?php
+    $username = "root"; 
+    $password = "admin";   
+    $host = "localhost";
+    $database="MTM";
+    
+    $server = mysql_connect($host, $username, $password);
+    $connection = mysql_select_db($database, $server);
+
+    $myquery = "
+SELECT * FROM DataLHR
+";
+    $query = mysql_query($myquery);
+    
+    if ( ! $query ) {
+        echo mysql_error();
+        die;
+    }
+    
+    $data = array();
+    
+    for ($x = 0; $x < mysql_num_rows($query); $x++) {
+        $data[] = mysql_fetch_assoc($query);
+    }
+    
+    echo json_encode($data);     
+     
+    mysql_close($server);
+?>
